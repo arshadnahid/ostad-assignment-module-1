6730def977eaf7515c9118b5ec690cd75ac12a68
arshadnahid <48785852+arshadnahid@users.noreply.github.com>
2023-09-13 22:18:10 +0600

    Personal Information

---
+++ personal_info.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Personal Information</title>
+</head>
+<body>
+<?php
+// Define variables with your personal information
+$name = "Your Name";
+$age = 25; // Update with your age
+$country = "Your Country";
+$introduction = "Hello, I'm Your Name. I'm $age years old and I come from $country.";
+
+// Display the personal information using the echo statement
+echo "<h1>Personal Information</h1>";
+echo "<p><strong>Name:</strong> $name</p>";
+echo "<p><strong>Age:</strong> $age</p>";
+echo "<p><strong>Country:</strong> $country</p>";
+echo "<p><strong>Introduction:</strong> $introduction</p>";
+?>
+</body>
+</html>
